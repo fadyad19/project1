1b7251e7337f681983eee8c3b5b197d534a2f2c3
fadyad19 <sim.fadyadel3089@alexu.edu.eg>
2022-04-01 21:45:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/project/scheduleClasses.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scheduleClasses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_classes);
+    }
+}
